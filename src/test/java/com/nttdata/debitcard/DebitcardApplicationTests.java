ebba69cf206c0e91a3dcb1294dc696e8a6e6ef49
Eduardo Orrego <corregoa@everis.com>
2024-03-11 16:42:25 -0500

    first commit

---
+++ src/test/java/com/nttdata/debitcard/DebitcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.debitcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
